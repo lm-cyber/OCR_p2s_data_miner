1acdb7621b3b1e97ea49b4a1458f9c21a115671f
Nasibullin Alan Maratovich <alan.nasibyllin@yandex.ru>
2024-11-11 15:02:07 +0300

    fix

--- service/main.py
+++ service/main.py
@@ -1,7 +1,7 @@
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-
+import uuid
 # Define the folder where images will be saved
 SAVE_FOLDER = "/app/images"  # This will be mounted to a host folder in Docker
 
@@ -10,15 +10,28 @@ os.makedirs(SAVE_FOLDER, exist_ok=True)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! Send me an image, and I will save it.")
-
 async def save_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.photo:
-        # Get the highest quality photo from the message
-        photo = update.message.photo[-1]
-        # Download the photo to the specified folder
-        file_path = os.path.join(SAVE_FOLDER, f"{photo.file_unique_id}.jpg")
-        await photo.get_file().download(file_path)
-        await update.message.reply_text("Image saved successfully!")
+    
+    file = update.message.photo[-1].file_id
+    obj = await context.bot.get_file(file)
+    file_path = os.path.join(SAVE_FOLDER, f"{uuid.uuid4()}.jpg")
+
+    await obj.download_to_drive(file_path)
+
+    # await file.download_to_drive(SAVE_FOLDER)
+    # if update.message.photo:
+    #     # Get the highest quality photo from the message
+    #     file = await update.message.photo[-1].get_file()
+    #     print(type(file),file)
+        # # First, await photo.get_file() to retrieve the file
+        # file_path = os.path.join(SAVE_FOLDER, f"{photo}.jpg")
+
+        # file = await photo.download(file_path)
+
+        # Now download the file
+        # await file.download(file_path)
+        # await update.message.reply_text("Image saved successfully!")
+
 
 def main():
     bot_token = os.getenv("BOT_TOKEN")  # Token from environment variable
